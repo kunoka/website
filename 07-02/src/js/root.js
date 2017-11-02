8467b508460149a25464faa6acc992070e2f4d68
Harry <hengwang@cn.ibm.com>
2017-11-02 08:50:01 +0800

    12-01-end

--- 07-02/src/js/root.js
+++ 07-02/src/js/root.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Router, Route, hashHistory} from 'react-router'
 import {Button} from 'antd'
+import PCIndex from './components/pc_index'
 import 'antd/dist/antd.css'
+
 class Root extends React.Component{
   render(){
     return (
       // 这里替换了之前的 Index，变成了程序的入口
       <div>
-
+        <PCIndex></PCIndex>
       </div>
     );
   }
