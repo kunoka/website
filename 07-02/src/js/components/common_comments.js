32ea5467a59bf07a1d0bbba7af2ce53a32f23b6f
Harry <hengwang@cn.ibm.com>
2017-11-28 08:25:36 +0800

    15-6 end commentList

--- 07-02/src/js/components/common_comments.js
+++ 07-02/src/js/components/common_comments.js
@@ -16,18 +16,34 @@ class CommonComments extends React.Component {
     }
   }
 
+  componentDidMount() {
+    let myFetchOptions = {
+      "method": 'GET'
+    }
+    let uri = "http://newsapi.gugujiankong.com/Handler.ashx?action=getcomments&uniquekey=" + this.props.params.uniquekey
+    fetch(uri, myFetchOptions).then(response => response.json()).then(json => this.setState({'comments': json}))
+  }
   handleSubmit (){
 
   }
   render(){
     let {getFieldProps} = this.props.form
+
+    const {comments} = this.state
+    const commentList = comments.length ?
+      comments.map((comment, index) => {<card index={index} title ={comment.UserName} extra={<a href="#">发布于{comment.datetime}</a>}>
+        <p>{comment.Comments}</p>
+      </card>})
+      :
+      '没有加载到任何评论'
     return(
       <div className="comment">
         <Row>
           <Col span="24">
+            {commentList}
             <Form onsubmit={this.handleSubmit.bind(this)}>
               <FormItem label="您的评论">
-                  <Input type="textarea" placeholder="请评论" {...getFieldProps('remark',{initialValue:''})}/>
+                <Input type="textarea" placeholder="请评论" {...getFieldProps('remark',{initialValue:''})}/>
               </FormItem>
               <Button type="primary" htmlType="submit">提交评论</Button>
             </Form>
