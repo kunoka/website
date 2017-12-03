3846af589e4b8578c58488efdd07b8480211d6f1
Harry <hengwang@cn.ibm.com>
2017-12-04 07:52:58 +0800

    15-9 end

--- 07-02/src/js/components/common_comments.js
+++ 07-02/src/js/components/common_comments.js
@@ -9,7 +9,7 @@ const MenuItemGroup = Menu.ItemGroup
 import {Router, Route, Link, browseHistory} from 'react-router'
 
 class CommonComments extends React.Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
       comments: ''
@@ -23,7 +23,8 @@ class CommonComments extends React.Component {
     let uri = "http://newsapi.gugujiankong.com/Handler.ashx?action=getcomments&uniquekey=" + this.props.uniquekey
     fetch(uri, myFetchOptions).then(response => response.json()).then(json => this.setState({comments: json}))
   }
-  handleSubmit (e){
+
+  handleSubmit(e) {
     e.preventDefault()
     let myFetchOptions = {
       "method": 'GET'
@@ -35,30 +36,45 @@ class CommonComments extends React.Component {
     fetch(uri, myFetchOptions)
       .then(response => response.json())
       .then(json => {
-      this.componentDidMount()
-    })
+        this.componentDidMount()
+      })
   }
 
-  render(){
+  addUserCollection() {
+    let myFetchOption = {
+      method: 'GET'
+    }
+    let uri = "http://newsapi.gugujiankong.com/Handler.ashx?action=uc&userid=" + localStorage.userid + "&uniquekey=" + this.props.uniquekey
+    fetch(uri, myFetchOption)
+      .then(response => response.json())
+      .then(json => {
+        // 收藏成功以后，进行全局提醒
+
+      })
+  }
+
+  render() {
     let {getFieldProps} = this.props.form
     const {comments} = this.state
     const commentList = comments.length ?
       comments.map((comment, index) => (
-        <Card key={index} title ={comment.UserName} extra={<a href="#">发布于{comment.datetime}</a>}>
-        <p>{comment.Comments}</p>
-      </Card>))
+        <Card key={index} title={comment.UserName} extra={<a href="#">发布于{comment.datetime}</a>}>
+          <p>{comment.Comments}</p>
+        </Card>))
       :
       '没有加载到任何评论'
-    return(
+    return (
       <div className="comment">
         <Row>
           <Col span="24">
             {commentList}
             <Form onSubmit={this.handleSubmit.bind(this)}>
               <FormItem label="您的评论">
-                <Input type="textarea" placeholder="请评论" {...getFieldProps('remark',{initialValue:''})}/>
+                <Input type="textarea" placeholder="请评论" {...getFieldProps('remark', {initialValue: ''})}/>
               </FormItem>
               <Button type="primary" htmlType="submit">提交评论</Button>
+              &nbsp;&nbsp;
+              <Button type='primary' htmlType='button' onClick={this.addUserCollection()}>收藏该文章</Button>
             </Form>
           </Col>
         </Row>
@@ -66,4 +82,5 @@ class CommonComments extends React.Component {
     )
   }
 }
+
 export default CommonComments = Form.create({})(CommonComments)
