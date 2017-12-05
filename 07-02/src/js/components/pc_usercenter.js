3685b55fbd21919bfc3d5c853a7d7f9247ce2ee5
Harry <hengwang@cn.ibm.com>
2017-12-05 08:54:38 +0800

    15-6-1 upload end

--- 07-02/src/js/components/pc_usercenter.js
+++ 07-02/src/js/components/pc_usercenter.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Row, Col} from 'antd'
-import {Menu, Icon, Tabs, message, Form, Input, Button, Checkbox, Modal, Card, notification} from 'antd'
+import {Menu, Icon, Tabs, message, Form, Input, Button, Checkbox, Modal, Card, notification, Upload} from 'antd'
 
 const FormItem = Form.Item
 const SubMenu = Menu.SubMenu
@@ -11,7 +11,40 @@ import PCHeader from './pc_header'
 import PCFooter from './pc_footer'
 
 export default class PCUserCenter extends React.Component {
+  // 构造
+    constructor(props) {
+      super(props);
+      // 初始状态
+      this.state = {
+        previewImage: '',
+        previewVisible: false
+      }
+    }
+
   render() {
+    const props = {
+      name: '',
+      action: 'http://newsapi.gugujiankong.com/handler.ashx',
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      },
+      listType: 'picture-card',
+      defaultFileList: [
+        {
+          uid: -1,
+          name: 'xxx.png',
+          state: 'done',
+          url: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
+          thumbUrl: 'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png'
+        }
+      ],
+      onPreview: (file) => {
+        this.setState({
+          previewImage: file.url,
+          previewVisible: true
+        })
+      }
+    }
     return (
       <div>
         <PCHeader/>
@@ -24,6 +57,16 @@ export default class PCUserCenter extends React.Component {
               <TabPane tab="我的评论列表" key="2">
               </TabPane>
               <TabPane tab="头像设置" key="3">
+                <div className="clearfix">
+                  <Upload {...props}>
+                    <Icon type="plus"/>
+                    <div className="ant-upload-text">上传照片</div>
+                  </Upload>
+                  <Modal visible={this.state.previewVisible} footer={null}
+                         onCancel={this.handleCancel}>
+                    <img src={this.state.previewImage} alt="预览"/>
+                  </Modal>
+                </div>
               </TabPane>
             </Tabs>
           </Col>
